6f351ea194540747530dcf04d87f19ba7eea0baf
kubzer0 <jakub.gryka@gmail.com>
2018-11-25 16:59:07 +0100

    fixed error at line 21

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class Square extends React.Component {
         className="square"
         onClick ={()=> this.setState({value: 'X'})}
         >
-          {this.props.value}
+          {this.state.value}
         </button>
       )
     }
